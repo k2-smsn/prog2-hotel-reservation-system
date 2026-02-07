8e3354613851752bb4660559fdab53563c28ab1d
k2 <kkvs1805@gmail.com>
2026-02-07 23:06:48 +0800

    initial commit

---
+++ src/main/java/main/HotelReservationSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author k2
+ */
+public class HotelReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
